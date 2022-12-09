b68af77fcec37e160b6660929ce335f2aa5fd65a
WiseChrispin <99377345+WiseChrispin@users.noreply.github.com>
2022-12-09 10:31:10 -0500

    Update Parking.java

--- OOP_3200_JAVA_LAB_3/src/Parking.java
+++ OOP_3200_JAVA_LAB_3/src/Parking.java
@@ -1,4 +1,45 @@
-
-public class Parking {
-
+import java.util.Vector;
+	
+public class Parking extends vehicle {
+	private String address;
+	Vector<vehicle> garage;
+	
+	// Constructors
+	public Parking()
+	{
+		setAddress("2000 Simcoe St. N. Oshawa, ON, Canada L1G 0C5");
+		//set vector
+		
+	}
+	
+	
+	
+	// Getters
+	public String getAddress()
+	{
+		return address;
+	}
+	
+	//get vector
+	
+	// Setters
+	public void setAddress(String address)
+	{
+		
+			this.address = address;
+		
+	}
+	
+	//setvector
+	
+	//member methods
+	//park vehicle function
+	//return vehicle function
+	//list vehicle function
+	//find vehicle function
+	//close parking function
+	//
+	
+	
+	
 }
