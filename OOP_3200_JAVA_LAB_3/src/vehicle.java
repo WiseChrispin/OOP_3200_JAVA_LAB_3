5b5697d4f52ce9d1a4d2aac3cb15b2e23562be7e
WiseChrispin <99377345+WiseChrispin@users.noreply.github.com>
2022-12-09 10:31:43 -0500

    Update vehicle.java

--- OOP_3200_JAVA_LAB_3/src/vehicle.java
+++ OOP_3200_JAVA_LAB_3/src/vehicle.java
@@ -1,4 +1,109 @@
 
 public class vehicle {
+	
 
+	
+
+	
+		protected int make;
+		protected String module;
+		protected String plate;
+		protected String colour;
+		
+		// Constructors
+		public vehicle()
+		{
+			setMake(2022);
+			setModule("Generic");
+			setPlate("D3f4u1T");
+			setColour("Black");
+			
+		}
+		
+		public vehicle(int make, String module, String plate, String colour)
+		{
+			setMake(make);
+			setModule(module);
+			setPlate(plate);
+			setColour(colour);
+		}
+		
+		// Getters
+		public int getMake()
+		{
+			return make;
+		}
+		
+		public String getModule()
+		{
+			return module;
+		}
+		
+		public String getPlate()
+		{
+			return plate;
+		}
+		
+		public String getColour()
+		{
+			return colour;
+		}
+		
+		
+		// Setters
+		public void setMake(int make)
+		{
+			if(make >1900 && make < 2023) 
+			{
+				this.make = make;
+			}
+		}
+		
+		public void setModule(String module)
+		{
+			this.module = module;
+		}
+		
+		public void setPlate(String plate) 
+		{
+			if(plate.length() > 1 && plate.length() < 9)
+			{
+				this.plate = plate;
+			}
+		}  
+		
+		public void setColour(String colour)
+		{
+			
+			
+				this.colour = colour;
+			
+		}
+		
+		//member methods
+		public String fillPadding(String src,int width)
+		{
+			while (src.length() < width)
+			{
+				src+=" ";
+			}
+			return src;
+		}
+		
+		public void display(boolean display) {
+			if(display == false)
+			{
+				
+				System.out.println("Module: " + getModule() + ", Make: " + getMake() + ", LicensePlate: " + getPlate() + ", Colour:" + getColour());
+				
+			}else
+			{			
+					
+					
+					System.out.println(fillPadding(getModule(),8) + "|" 
+					+ fillPadding(Integer.toString(getMake()),11) + "|" + fillPadding(getPlate(),8) + "|" 
+					+ fillPadding(getColour(),22));
+			}
+		}
+	}
 }
